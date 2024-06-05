ba5f3faf40bcfabed31a345f7db38c38411ff875
sauloalbuquerque01 <167372681+sauloalbuquerque01@users.noreply.github.com>
2024-06-05 20:35:48 -0300

    Update problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -1,10 +1,1 @@
-def soma_sequencia_ate_n(n):
-    return sum(range(n + 1))
 
-def main():
-    ultimo_numero = int(input("Digite o último número da sequência: "))
-    resultado = soma_sequencia_ate_n(ultimo_numero)
-    print("A soma da sequência até", ultimo_numero, "é:", resultado)
-
-if __name__ == "__main__":
-    main()
