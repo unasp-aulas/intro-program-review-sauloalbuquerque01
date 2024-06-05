6d084719f94b07568c06908caf0c80c87ca52ac8
sauloalbuquerque01 <167372681+sauloalbuquerque01@users.noreply.github.com>
2024-06-05 20:52:22 -0300

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -1,18 +1,15 @@
 salario = float(input("Qual é o seu salario: "))
 cargo = input("Qual é o seu cargo: ")
-    
+
 def main(salario, cargo):
     if cargo == "junior":
-        return salario * 1.15  
+        novo_salario = salario * 1.15  
     elif cargo == "pleno":
-        return salario * 1.26 
+        novo_salario = salario * 1.26 
     elif cargo == "senior":
-        return salario * 1.34 
+        novo_salario = salario * 1.34 
     else:
-        return -1  
-        
-    novo_salario = calcular_aumento(salario, cargo)
-    if novo_salario == -1:
-        return("Cargo invalido!")
-
+        return "Cargo invalido!"
+    
+    return f"O novo salário é: {novo_salario:.2f}"
 
